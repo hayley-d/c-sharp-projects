8e78904198b24b8c28a65e42e8f4e707099d6f87
hayley-d <hayleydod@proton.me>
2025-06-20 06:41:09 +0200

    complete day 1

--- AdventOfCode/AdventOfCodeDay1/AdventOfCodeDay1/Program.cs
+++ AdventOfCode/AdventOfCodeDay1/AdventOfCodeDay1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCodeDay1
 {
@@ -38,6 +39,40 @@ namespace AdventOfCodeDay1
                     total += diff;
                 }
                 Console.WriteLine(total);
+                Console.WriteLine("Starting Phase 2");
+                PartTwo(lines);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void PartTwo(string[] inputLines) {
+            try{
+                List<int> col1 = new List<int>();
+                List<int> col2 = new List<int>();
+
+                foreach(string line in inputLines) {
+                    string[] numbers = line.Split("   ");
+
+                    if(numbers.Length == 2) {
+                        col1.Add(Int32.Parse(numbers[0]));
+                        col2.Add(Int32.Parse(numbers[1]));
+                    } else {
+                        throw new CustomException("Invalid Array Length Part 2");
+                    }
+                }
+
+                var counts = col1.Distinct().Select(x => new {
+                        Value = x,
+                        Count = col2.Count(y => y==x)
+                });
+
+                int similarityScore = 0;
+                foreach(var assesment in counts) {
+                    similarityScore += (assesment.Value * assesment.Count);
+                }
+
+                Console.WriteLine(similarityScore);
             } catch (Exception ex) {
                 Console.WriteLine(ex.Message);
             }
