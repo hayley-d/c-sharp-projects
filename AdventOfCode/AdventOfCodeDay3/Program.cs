4ac43ce2c311142f0b3a5da404592e5c089f11ef
hayley-d <hayleydod@proton.me>
2025-06-20 13:12:31 +0200

    will come back to this

--- AdventOfCode/AdventOfCodeDay3/Program.cs
+++ AdventOfCode/AdventOfCodeDay3/Program.cs
@@ -28,6 +28,34 @@ namespace AdventOfCodeDay3 {
         }
 
         static void PartTwo(string input) {
+            List<int> dos = new List<int>();
+            List<int> donts = new List<int>();
+            List<long> totals = new List<long>();
+
+            //Start off enabled
+            dos.Add(0);
+            for(int i = 0;;i += "do()".Length) {
+                int index = input.IndexOf("do()", i);
+                if (index == -1) break;
+                dos.Add(index);
+            }
+
+            for(int i = 0 ;; i += "don't()".Length) {
+                int index  = input.IndexOf("don't()", i);
+                if (index == -1) break;
+                donts.Add(index);
+            }
+
+            
+            for(int i = 0; i < donts.Count; i += 1) {
+                if(i >= dos.Count) {
+                    break;
+                }
+                int end = donts[i];
+                int start = dos.Where(n => n < donts[i]).Last();
+                totals.Add(Regex.Matches(input.Substring(start,end), @"mul\((\d{1,3}),(\d{1,3})\)").Select(m => long.Parse(m.Groups[1].Value) * long.Parse(m.Groups[2].Value)).Sum());
+
+            }
         }
     }
 }
