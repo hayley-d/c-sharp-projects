52196b8eaa2ee6f709e8253abcf3afbf3506a1c2
hayley-d <hayleydod@proton.me>
2025-06-20 05:17:44 +0200

    test comment

--- TestHttpApp/Program.cs
+++ TestHttpApp/Program.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 namespace TestHttpApp {
     class Program {
         static void Main(string[] args) {
+            // Read all lines in the file into an array
             string[] inputLines = File.ReadAllLines("input.txt");
 
             foreach(string line in inputLines) {
