f2588839803f66e232beccb1f41d34b0d5bdb396
hayley-d <hayleydod@proton.me>
2025-06-20 06:27:14 +0200

    finished part 1

---
+++ AdventOfCode/AdventOfCodeDay1/AdventOfCodeDay1/CustomException.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace AdventOfCodeDay1 {
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message)
+            : base(message)
+        {
+        }
+
+        public CustomException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCodeDay1/AdventOfCodeDay1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCodeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                List<int> col1 = new List<int>();
+                List<int> col2 = new List<int>();
+
+                List<int> diffs = new List<int>();
+                string[] lines  = File.ReadAllLines("input.txt");
+                foreach (string line in lines)
+                {
+                    string[] StringNumbers = line.Split("   ");
+                    if (StringNumbers.Length == 2) {
+                        col1.Add(Int32.Parse(StringNumbers[0]));
+                        col2.Add(Int32.Parse(StringNumbers[1]));
+                    } else {
+                        throw new CustomException("Invalid array length");
+                    }
+                }
+
+                //Sort the lists
+                col1.Sort();
+                col2.Sort();
+
+                for (int i = 0; i < col1.Count; i++) {
+                    diffs.Add(Math.Abs(col1[i] - col2[i]));
+                }
+
+                int total = 0;
+                foreach(int diff in diffs) {
+                    total += diff;
+                }
+                Console.WriteLine(total);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
