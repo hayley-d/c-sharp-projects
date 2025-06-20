9b72999047c2db11772e5eb4008273ac58fed746
hayley-d <hayleydod@proton.me>
2025-06-20 12:34:25 +0200

    fixed part 1

--- AdventOfCode/AdventOfCodeDay3/Program.cs
+++ AdventOfCode/AdventOfCodeDay3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCodeDay3 {
     class Program {
@@ -7,7 +8,7 @@ namespace AdventOfCodeDay3 {
             try{
                 if(args.Length > 0) {
                    string input = File.ReadAllText(args[0]);
-                   PartOne(input.ToCharArray());
+                   PartOne(input);
                    PartTwo(input);
                 } else {
                     throw new Exception("No file name included in command line args");
@@ -17,66 +18,10 @@ namespace AdventOfCodeDay3 {
             }
         }
 
-        static void PartOne(char[] input) {
+        static void PartOne(string input) {
             try {
-                List<char> buffer = new List<char>();
-                List<long> answers = new List<long>();
-
-                for(int i = 0; i < input.Length; i++) {
-                   if (input[i] is 'm') {
-                        bool isCorrectSyntax = true;
-                        long num1 = 0;
-                        long num2 = 0;
-                        if(input[i+1] is 'u' && input[i+2] is'l' && input[i+3] is '(') {
-                            i += 4;
-                            while(isCorrectSyntax && i < input.Length) {
-                                if(Char.IsDigit(input[i])) {
-                                    buffer.Add(input[i]); 
-                                } else if (input[i] is ',') {
-                                    break;
-                                } else {
-                                    isCorrectSyntax = false;
-                                    break;
-                                }
-                                i += 1;
-                            }
-
-                            if (isCorrectSyntax) {
-                                if(buffer.Count > 3 || buffer.Count < 1) {
-                                    isCorrectSyntax = false;
-                                    continue;
-                                }
-                                i += 1;
-                                num1 = Int64.Parse(new string(buffer.ToArray()));
-                                buffer = new List<char>();
-                            }
-                            while(isCorrectSyntax && i < input.Length) {
-                                if(Char.IsDigit(input[i])) {
-                                    buffer.Add(input[i]); 
-                                } else if (input[i] is ')') {
-                                    break;
-                                } else {
-                                    isCorrectSyntax = false;
-                                    break;
-                                }
-                                i += 1;
-                            }
-
-                            if (isCorrectSyntax) {
-                                if(buffer.Count > 3 || buffer.Count < 1) {
-                                    isCorrectSyntax = false;
-                                    continue;
-                                }
-                                num2 = Int64.Parse(new string(buffer.ToArray()));
-                                answers.Add(num1 * num2);
-                            }
-                        }
-                   } else {
-                       continue;
-                   }
-                }
-                long total = answers.Sum();
-                Console.WriteLine("Sum of multiplication: " + total);
+                long total = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)").Select(m => long.Parse(m.Groups[1].Value) * long.Parse(m.Groups[2].Value)).Sum();
+                Console.WriteLine(total);
             } catch (Exception ex) {
                 Console.WriteLine(ex.Message);
             }
